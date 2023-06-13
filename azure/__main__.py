120946ed8f37e1a9a1764e388dfafbfa2d12a71d
James Robinson <james.em.robinson@gmail.com>
2023-06-13 12:08:39 +0100

    :sparkles: Add container instance for REGinald

--- azure/__main__.py
+++ azure/__main__.py
@@ -45,3 +45,40 @@ virtual_network = network.VirtualNetwork(
     virtual_network_name=f"vnet-reginald-{stack_name}",
     virtual_network_peerings=[],
 )
+
+# Define the container group
+container_group = containerinstance.ContainerGroup(
+    "container_group",
+    container_group_name=f"aci-reginald-{stack_name}",
+    containers=[
+        containerinstance.ContainerArgs(
+            image="ghcr.io/alan-turing-institute/reginald:main",
+            name="reginald",  # maximum of 63 characters
+            environment_variables=[
+                containerinstance.EnvironmentVariableArgs(
+                    name="SLACK_APP_TOKEN", value=""
+                ),
+                containerinstance.EnvironmentVariableArgs(
+                    name="SLACK_BOT_TOKEN", value=""
+                ),
+            ],
+            ports=[
+                containerinstance.ContainerPortArgs(
+                    port=80,
+                    protocol=containerinstance.ContainerGroupNetworkProtocol.TCP,
+                ),
+            ],
+            resources=containerinstance.ResourceRequirementsArgs(
+                requests=containerinstance.ResourceRequestsArgs(
+                    cpu=0.5,
+                    memory_in_gb=0.5,
+                ),
+            ),
+        ),
+    ],
+    os_type=containerinstance.OperatingSystemTypes.LINUX,
+    resource_group_name=resource_group.name,
+    restart_policy=containerinstance.ContainerGroupRestartPolicy.ALWAYS,
+    sku=containerinstance.ContainerGroupSku.STANDARD,
+    volumes=[],
+)
