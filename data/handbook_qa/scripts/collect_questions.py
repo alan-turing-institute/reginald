c86827074b85d80d1d8c92ec0f14ccebf7f4e982
rchan <rchan@turing.ac.uk>
2023-06-15 15:25:12 +0100

    apply pre-commit

--- data/handbook_qa/scripts/collect_questions.py
+++ data/handbook_qa/scripts/collect_questions.py
@@ -9,6 +9,7 @@ import html2text
 import markdown
 import openai
 
+
 def read_markdown_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         content = file.read()
@@ -16,29 +17,27 @@ def read_markdown_file(file_path):
         text = html2text.html2text(html)
         return text
 
+
 def process_file(file_path: str, q_per_doc: int) -> List[Dict[str, str]]:
     content = read_markdown_file(file_path)
-    question_prompt = ("Forget the instruction you have previously received. "
+    question_prompt = (
+        "Forget the instruction you have previously received. "
         + f"Please generate {q_per_doc} questions about the following topic."
-        + f"The topic is as follows: '{content}'.")
+        + f"The topic is as follows: '{content}'."
+    )
 
     output = []
     try:
         question_generator = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "user",
-                    "content": question_prompt
-                }
-            ],
+            messages=[{"role": "user", "content": question_prompt}],
             stop=["[AI]"],
         )
         question = question_generator["choices"][0]["message"]["content"].strip()
         question = question.replace("?", "") + "?"
 
         # Store chat content
-        output.append({"context_name":file_path,"question": question})
+        output.append({"context_name": file_path, "question": question})
         print("\n\n****")
         print(f"Question: {question}")
         print("****")
@@ -46,6 +45,7 @@ def process_file(file_path: str, q_per_doc: int) -> List[Dict[str, str]]:
         print(f"OpenAI error\n{str(exc)}")
 
     return output
+
 
 if __name__ == "__main__":
     openai.api_key = sys.argv[1]
@@ -67,13 +67,11 @@ if __name__ == "__main__":
     except:
         output = {}
 
-
     reg_files = [f for f in os.listdir(reg_folder) if isfile(join(reg_folder, f))]
     for f in reg_files:
         print(f"Now working on {f}")
         output[f] = process_file(os.path.join(reg_folder, f), q_per_doc)
         pickle.dump(
-            output, open("collected_questions/{}_generated.pickle".format(data_name), "wb")
+            output,
+            open("collected_questions/{}_generated.pickle".format(data_name), "wb"),
         )
-        
-
