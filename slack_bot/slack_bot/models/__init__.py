180a765c7bc3add450ba4bc2746b1eb62741bb05
James Robinson <james.em.robinson@gmail.com>
2023-06-15 11:44:47 +0100

    :truck: Renamed OpenAI models to ChatCompletion

--- slack_bot/slack_bot/models/__init__.py
+++ slack_bot/slack_bot/models/__init__.py
@@ -1,16 +1,15 @@
 from .base import ResponseModel
 from .hello import Hello
-from .llama import LlamaDistilGPT2, LlamaGPT35TurboPersonal, LlamaGPT35TurboAzure
-from .openai_azure import OpenAIAzure
-from .openai_personal import OpenAIPersonal
+from .llama import LlamaDistilGPT2, LlamaGPT35TurboOpenAI, LlamaGPT35TurboAzure
+from .chat_completion import ChatCompletionAzure, ChatCompletionOpenAI
 
 MODELS = {
     "hello": Hello,
     "llama-distilgpt2": LlamaDistilGPT2,
     "llama-gpt-3.5-turbo-azure": LlamaGPT35TurboAzure,
-    "llama-gpt-3.5-turbo-personal": LlamaGPT35TurboPersonal,
-    "openai-azure": OpenAIAzure,
-    "openai-personal": OpenAIPersonal,
+    "llama-gpt-3.5-turbo-openai": LlamaGPT35TurboOpenAI,
+    "chat-completion-azure": ChatCompletionAzure,
+    "chat-completion-openai": ChatCompletionOpenAI,
 }
 
 __all__ = ["MODELS", "ResponseModel"]
