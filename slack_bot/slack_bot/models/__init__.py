f20597000a310bdc6db442fbefc2ddb6bb3c9b87
James Robinson <james.em.robinson@gmail.com>
2023-06-15 11:57:12 +0100

    :rotating_light: Reordered imports

--- slack_bot/slack_bot/models/__init__.py
+++ slack_bot/slack_bot/models/__init__.py
@@ -1,17 +1,17 @@
 from .base import ResponseModel
-from .hello import Hello
-from .llama import LlamaDistilGPT2, LlamaGPT35TurboOpenAI, LlamaGPT35TurboAzure
 from .chat_completion import ChatCompletionAzure, ChatCompletionOpenAI
+from .hello import Hello
+from .llama import LlamaDistilGPT2, LlamaGPT35TurboAzure, LlamaGPT35TurboOpenAI
 
 # Please ensure that any models needing OPENAI_API_KEY are named *openai*
 # Please ensure that any models needing OPENAI_AZURE_API_BASE and OPENAI_AZURE_API_KEY are named *azure*
 MODELS = {
+    "chat-completion-azure": ChatCompletionAzure,
+    "chat-completion-openai": ChatCompletionOpenAI,
     "hello": Hello,
     "llama-distilgpt2": LlamaDistilGPT2,
     "llama-gpt-3.5-turbo-azure": LlamaGPT35TurboAzure,
     "llama-gpt-3.5-turbo-openai": LlamaGPT35TurboOpenAI,
-    "chat-completion-azure": ChatCompletionAzure,
-    "chat-completion-openai": ChatCompletionOpenAI,
 }
 
 __all__ = ["MODELS", "ResponseModel"]
