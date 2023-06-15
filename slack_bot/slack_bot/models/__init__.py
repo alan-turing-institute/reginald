3f456715458aa833880cad1a66f4b3c5015e1271
James Robinson <james.em.robinson@gmail.com>
2023-06-15 11:40:01 +0100

    :recycle: Only prompt for the keys that we need

--- slack_bot/slack_bot/models/__init__.py
+++ slack_bot/slack_bot/models/__init__.py
@@ -1,16 +1,16 @@
 from .base import ResponseModel
 from .hello import Hello
-from .llama import LlamaDistilGPT2, LlamaGPT35Turbo, LlamaGPT35TurboAzure
+from .llama import LlamaDistilGPT2, LlamaGPT35TurboPersonal, LlamaGPT35TurboAzure
 from .openai_azure import OpenAIAzure
 from .openai_personal import OpenAIPersonal
 
 MODELS = {
     "hello": Hello,
     "llama-distilgpt2": LlamaDistilGPT2,
-    "llama-gpt-3.5-turbo": LlamaGPT35Turbo,
     "llama-gpt-3.5-turbo-azure": LlamaGPT35TurboAzure,
-    "openai_azure": OpenAIAzure,
-    "openai_personal": OpenAIPersonal,
+    "llama-gpt-3.5-turbo-personal": LlamaGPT35TurboPersonal,
+    "openai-azure": OpenAIAzure,
+    "openai-personal": OpenAIPersonal,
 }
 
 __all__ = ["MODELS", "ResponseModel"]
