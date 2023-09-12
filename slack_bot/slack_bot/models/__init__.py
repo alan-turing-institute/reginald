6566b02cd233c24eb2a5b163211137ac6e87f2b3
rchan <rchan@turing.ac.uk>
2023-09-12 10:12:05 +0100

    rename Llama... to LlamaIndex... models

--- slack_bot/slack_bot/models/__init__.py
+++ slack_bot/slack_bot/models/__init__.py
@@ -1,7 +1,7 @@
 from .base import ResponseModel
 from .chat_completion import ChatCompletionAzure, ChatCompletionOpenAI
 from .hello import Hello
-from .llama import LlamaGPT35TurboAzure, LlamaGPT35TurboOpenAI, LlamaHuggingFace
+from .llama import LlamaIndexGPTAzure, LlamaIndexGPTOpenAI, LlamaIndexHF
 
 # Please ensure that any models needing OPENAI_API_KEY are named *openai*
 # Please ensure that any models needing OPENAI_AZURE_API_BASE and OPENAI_AZURE_API_KEY are named *azure*
@@ -9,9 +9,9 @@ MODELS = {
     "chat-completion-azure": ChatCompletionAzure,
     "chat-completion-openai": ChatCompletionOpenAI,
     "hello": Hello,
-    "llama-huggingface": LlamaHuggingFace,
-    "llama-gpt-3.5-turbo-azure": LlamaGPT35TurboAzure,
-    "llama-gpt-3.5-turbo-openai": LlamaGPT35TurboOpenAI,
+    "llama-index-hf": LlamaIndexHF,
+    "llama-index-gpt-3.5-turbo-azure": LlamaIndexGPTAzure,
+    "llama-index-gpt-3.5-turbo-openai": LlamaIndexGPTOpenAI,
 }
 
 __all__ = ["MODELS", "ResponseModel"]
