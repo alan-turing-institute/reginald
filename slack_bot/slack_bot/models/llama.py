61a4f577e263baf92713060a39bf512dc6945065
rchan <rchan@turing.ac.uk>
2023-09-12 10:12:05 +0100

    rename Llama... to LlamaIndex... models

--- slack_bot/slack_bot/models/llama.py
+++ slack_bot/slack_bot/models/llama.py
@@ -27,25 +27,52 @@ PUBLIC_DATA_DIR = "public"
 INTERNAL_DATA_DIR = "turing_internal"
 
 
-class Llama(ResponseModel):
+class LlamaIndex(ResponseModel):
     def __init__(
         self,
         model_name: str,
         max_input_size: int,
         data_dir: pathlib.Path,
         which_index: str,
-        context_window: int = 1024,
         chunk_size_limit: Optional[int] = None,
         k: int = 3,
         chunk_overlap_ratio: float = 0.1,
         force_new_index: bool = False,
         num_output: int = 256,
     ) -> None:
+        """
+        Base class for models using llama-index.
+        This class is not intended to be used directly, but rather subclassed
+        to implement the `_prep_llm` method which constructs the LLM to be used.
+
+        Parameters
+        ----------
+        model_name : str
+            Model name to specify which LLM to use.
+        max_input_size : int
+            Context window size for the LLM.
+        data_dir : pathlib.Path
+            Path to the data directory.
+        which_index : str
+            Which index to construct (if force_new_index is True) or use.
+            Options are "handbook", "public", or "all_data".
+        chunk_size_limit : Optional[int], optional
+            Maximum size of chunks to use, by default None.
+            If None, this is computed as `ceil(max_input_size / k)`.
+        k : int, optional
+            `similarity_top_k` to use in query engine, by default 3
+        chunk_overlap_ratio : float, optional
+            Chunk overlap as a ratio of chunk size, by default 0.1
+        force_new_index : bool, optional
+            Whether or not to recreate the index vector store,
+            by default False
+        num_output : int, optional
+            Number of outputs for the LLM, by default 256
+        """
         super().__init__(emoji="llama")
         logging.info("Setting up Huggingface backend.")
         self.max_input_size = max_input_size
         self.model_name = model_name
-        self.context_window = context_window
         self.num_output = num_output
         if chunk_size_limit is None:
             chunk_size_limit = math.ceil(max_input_size / k)
@@ -102,7 +129,7 @@ class Llama(ResponseModel):
                 storage_context=storage_context, service_context=service_context
             )
 
-        self.query_engine = self.index.as_query_engine(similarity_top_k=3)
+        self.query_engine = self.index.as_query_engine(similarity_top_k=k)
         logging.info("Done setting up Huggingface backend for query engine.")
 
         self.error_response_template = (
@@ -113,6 +140,20 @@ class Llama(ResponseModel):
 
     @staticmethod
     def _format_sources(response: RESPONSE_TYPE) -> str:
+        """
+        Method to format the sources used to compose the response.
+
+        Parameters
+        ----------
+        response : RESPONSE_TYPE
+            response object from the query engine
+
+        Returns
+        -------
+        str
+            String containing the formatted sources that
+            were used to compose the response
+        """
         texts = []
         for source_node in response.source_nodes:
             source_text = (
@@ -125,6 +166,22 @@ class Llama(ResponseModel):
         return result
 
     def _get_response(self, msg_in: str, user_id: str) -> str:
+        """
+        Method to obtain a response from the query engine given
+        a message and a user id.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        str
+            String containing the response from the query engine.
+        """
         try:
             query_response = self.query_engine.query(msg_in)
             # concatenate the response with the resources that it used
@@ -153,6 +210,14 @@ class Llama(ResponseModel):
         return answer
 
     def _prep_documents(self) -> List[Document]:
+        """
+        Method to prepare the documents for the index vector store.
+
+        Returns
+        -------
+        List[Document]
+            List of `llama_index.Documents` to be used to construct the index vector store.
+        """
         # Prep the contextual documents
         documents = []
 
@@ -200,30 +265,84 @@ class Llama(ResponseModel):
         return documents
 
     def _prep_llm(self) -> LLM:
+        """
+        Method to prepare the LLM to be used.
+
+        Returns
+        -------
+        LLM
+            LLM to be used.
+
+        Raises
+        ------
+        NotImplemented
+            This must be implemented by a subclass of LlamaIndex.
+        """
         raise NotImplemented(
-            "_prep_llm needs to be implemented by a subclass of Llama."
+            "_prep_llm needs to be implemented by a subclass of LlamaIndex."
         )
 
     def direct_message(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a direct message in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         backend_response = self._get_response(message, user_id)
+
         return MessageResponse(backend_response)
 
     def channel_mention(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a channel mention in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         backend_response = self._get_response(message, user_id)
+
         return MessageResponse(backend_response)
 
 
-class LlamaHuggingFace(Llama):
+class LlamaIndexHF(LlamaIndex):
     def __init__(
         self, model_name: str = "distilgpt2", *args: Any, **kwargs: Any
-    ) -> LLM:
+    ) -> None:
+        """
+        `LlamaIndexHF` is a subclass of `LlamaIndex` that uses HuggingFace's
+        `transformers` library to implement the LLM.
+
+        Parameters
+        ----------
+        model_name : str, optional
+            Model name from Huggingface's model hub, by default "distilgpt2".
+        """
         super().__init__(
             *args, model_name=model_name, max_input_size=self.num_output, **kwargs
         )
 
     def _prep_llm(self) -> LLM:
         return HuggingFaceLLM(
-            context_window=self.context_window,
+            context_window=self.max_input_size,
             max_new_tokens=self.num_output,
             tokenizer_name=self.model_name,
             model_name=self.model_name,
@@ -231,8 +350,14 @@ class LlamaHuggingFace(Llama):
         )
 
 
-class LlamaGPT35TurboOpenAI(Llama):
+class LlamaIndexGPTOpenAI(LlamaIndex):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """
+        `LlamaIndexGPTOpenAI` is a subclass of `LlamaIndex` that uses OpenAI's
+        API to implement the LLM.
+
+        Must have `OPENAI_API_KEY` set as an environment variable.
+        """
         self.openai_api_key = os.getenv("OPENAI_API_KEY")
         self.temperature = 0.7
         super().__init__(
@@ -248,9 +373,18 @@ class LlamaGPT35TurboOpenAI(Llama):
         )
 
 
-class LlamaGPT35TurboAzure(Llama):
+class LlamaIndexGPTAzure(LlamaIndex):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.deployment_name = "reginald-azure-gpt35-turbo"
+        """
+        `LlamaIndexGPTAzure` is a subclass of `LlamaIndex` that uses Azure's
+        instance of OpenAI's LLMs to implement the LLM.
+
+        Must have the following environment variables set:
+        - `OPENAI_API_BASE`: Azure endpoint which looks
+        like https://YOUR_RESOURCE_NAME.openai.azure.com/
+        - `OPENAI_API_KEY`: Azure API key
+        """
+        self.deployment_name = "reginald-azure-gpt-turbo"
         self.openai_api_base = os.getenv("OPENAI_AZURE_API_BASE")
         self.openai_api_key = os.getenv("OPENAI_AZURE_API_KEY")
         self.openai_api_version = "2023-03-15-preview"
