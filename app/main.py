1a9d632b6550171e95a54c6caecc4c437f4f4235
Rosie Wood <rwood@turing.ac.uk>
2023-09-22 08:58:06 +0100

    separate bot and llm

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from setup_llm import setup_llm
+
+
+class Query(BaseModel):
+    message: str
+    user_id: str
+
+
+response_model = setup_llm("hello")  # TODO: FIX HERE
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello World"
+
+
+@app.get("/direct_message")
+async def direct_message(query: Query):
+    response = response_model.direct_message(query.message, query.user_id)
+    return response
+
+
+@app.get("/channel_mention")
+async def channel_mention(query: BaseModel):
+    response = response_model.channel_mention(query.message, query.user_id)
+    return response
