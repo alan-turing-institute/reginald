b8d0bd29fdeb6682a788391f8ff6747ef9203ade
rchan <rchan@turing.ac.uk>
2023-09-25 18:05:21 +0100

    🗑️ remove file

--- reginald/models/testing.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-print(
-    pathlib.Path(
-        (pathlib.Path(__file__).parent.parent.parent / "data").resolve()
-    ).resolve
-)
-print((pathlib.Path(__file__).parent.parent.parent / "data").resolve())
