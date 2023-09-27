201097321897fec84434d0c1f14f74afb596739b
rchan <rchan@turing.ac.uk>
2023-09-27 17:21:33 +0100

    📝 update model docstrings

--- reginald/models/models/chat_completion.py
+++ reginald/models/models/chat_completion.py
@@ -16,6 +16,20 @@ class ChatCompletionAzure(ChatCompletionBase):
     def __init__(
         self, model_name: str = "reginald-curie", *args: Any, **kwargs: Any
     ) -> None:
+        """
+        Simple chat completion model using Azure's
+        instance of OpenAI's LLMs to implement the LLM.
+
+        Must have the following environment variables set:
+        - `OPENAI_API_BASE`: Azure endpoint which looks
+          like https://YOUR_RESOURCE_NAME.openai.azure.com/
+        - `OPENAI_API_KEY`: Azure API key
+
+        Parameters
+        ----------
+        model_name : str, optional
+            Deployment name of the model on Azure, by default "reginald-curie"
+        """
         logging.info(f"Setting up AzureOpenAI LLM (model {model_name})")
         super().__init__(*args, **kwargs)
         self.api_base = os.getenv("OPENAI_AZURE_API_BASE")
@@ -31,6 +45,21 @@ class ChatCompletionAzure(ChatCompletionBase):
         self.top_p = 0.95
 
     def _respond(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a message in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         openai.api_base = self.api_base
         openai.api_type = self.api_type
         openai.api_version = self.api_version
@@ -49,9 +78,39 @@ class ChatCompletionAzure(ChatCompletionBase):
         return MessageResponse(response["choices"][0]["text"])
 
     def direct_message(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a direct message in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         return self._respond(message=message, user_id=user_id)
 
     def channel_mention(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a channel mention in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         return self._respond(message=message, user_id=user_id)
 
 
@@ -59,11 +118,36 @@ class ChatCompletionOpenAI(ChatCompletionBase):
     def __init__(
         self, model_name: str = "gpt-3.5-turbo", *args: Any, **kwargs: Any
     ) -> None:
+        """
+        Simple chat completion model using OpenAI's API.
+
+        Must have `OPENAI_API_KEY` set as an environment variable.
+
+        Parameters
+        ----------
+        model_name : str, optional
+            Model name on OpenAI, by default "gpt-3.5-turbo"
+        """
         super().__init__(*args, **kwargs)
         self.model_name = model_name
         self.api_key = os.getenv("OPENAI_API_KEY")
 
     def _respond(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a message in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         openai.api_key = self.api_key
         response = openai.ChatCompletion.create(
             model=self.model_name, messages=[{"role": "user", "content": message}]
@@ -71,7 +155,37 @@ class ChatCompletionOpenAI(ChatCompletionBase):
         return MessageResponse(response["choices"][0]["message"]["content"])
 
     def direct_message(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a direct message in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         return self._respond(message=message, user_id=user_id)
 
     def channel_mention(self, message: str, user_id: str) -> MessageResponse:
+        """
+        Method to respond to a channel mention in Slack.
+
+        Parameters
+        ----------
+        msg_in : str
+            Message from user
+        user_id : str
+            User ID
+
+        Returns
+        -------
+        MessageResponse
+            Response from the query engine.
+        """
         return self._respond(message=message, user_id=user_id)
