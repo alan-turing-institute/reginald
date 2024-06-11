c5d219b3e79d9b93e88c64eb6f28e4efec0cdfab
Dr Griffith Rees <griff.rees@gmail.com>
2024-06-11 17:43:22 +0100

    fix(test): add passing `test_chat_cli` and `test_chat_interactive_exit`

--- test/test_chat_interact.py
+++ test/test_chat_interact.py
@@ -1,24 +1,25 @@
-import pytest
+import builtins
+from unittest import mock
 
+import pytest
+from typer.testing import CliRunner
+
+from reginald.cli import cli
 from reginald.models.models.hello import Hello
 from reginald.run import chat_interact
 
-
-def test_chat_interact(monkeypatch):
-    user_inputs = iter(["What's up?", "exit"])
-    interaction = chat_interact(model="hello")
-
-    monkeypatch.setattr("builtins.input", lambda name: next(user_inputs))
-    assert isinstance(interaction, Hello)
-
-    # captured = capsys.readouterr()
-    # assert "Reginald: Let's discuss this in a channel!" in captured.out
+runner = CliRunner()
 
 
-def test_chat_interact_exit(monkeypatch):
-    inputs = []
-    interaction = chat_interact(model="hello")
+def test_chat_cli():
+    """Test sending an input `str` via `cli` and then exiting."""
+    result = runner.invoke(cli, ["chat"], input="What's up dock?\nexit\n")
+    term_stdout_lines: list[str] = result.stdout.split("\n")
+    assert term_stdout_lines[0] == ">>> Reginald: Let's discuss this in a channel!"
+    assert term_stdout_lines[1] == ">>> "
 
-    monkeypatch.setattr("builtins.input", lambda name: next(inputs))
 
+def test_chat_interact_exit():
+    with mock.patch.object(builtins, "input", lambda _: "exit"):
+        interaction = chat_interact(model="hello")
     assert isinstance(interaction, Hello)
